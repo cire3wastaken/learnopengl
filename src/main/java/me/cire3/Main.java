6b521a13e4fbccb3b8cf6299a627f2beb8142b3e
cire3 <133830840+cire3wastaken@users.noreply.github.com>
2024-06-10 22:50:32 -0500

    skeleton code for glfw

--- src/main/java/me/cire3/Main.java
+++ src/main/java/me/cire3/Main.java
@@ -1,7 +1,38 @@
 package me.cire3;
 
+import org.lwjgl.*;
+import org.lwjgl.glfw.*;
+import org.lwjgl.opengl.*;
+import org.lwjgl.system.*;
+
+import static org.lwjgl.glfw.Callbacks.*;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryStack.*;
+import static org.lwjgl.system.MemoryUtil.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello LWJGL! - cire3");
+        initGlfw();
+    }
+
+    public static void initGlfw() {
+        GLFWErrorCallback.createPrint(System.err).set();
+
+        if (!glfwInit())
+            throw new RuntimeException("Failed to initialize GLFW!");
+
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+        long window = glfwCreateWindow(800, 600, "LearnOpenGl", NULL, NULL);
+        if (window == NULL) {
+            glfwTerminate();
+            throw new RuntimeException("Failed to create Window object!");
+        }
+
+        glfwMakeContextCurrent(window);
     }
 }
