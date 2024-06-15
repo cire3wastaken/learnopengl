5f033025d794e08c43521ffdec57be44e3a00b00
cire3 <133830840+cire3wastaken@users.noreply.github.com>
2024-06-14 22:50:28 -0500

    begin work on creating textures, finish tmr

--- src/main/java/me/cire3/lwjgl/objects/TextureGL.java
+++ src/main/java/me/cire3/lwjgl/objects/TextureGL.java
@@ -3,9 +3,14 @@ package me.cire3.lwjgl.objects;
 import me.cire3.App;
 import me.cire3.lwjgl.ObjectGL;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 
-import static org.lwjgl.opengl.GL11C.glDeleteTextures;
+import static org.lwjgl.opengl.GL11C.*;
 
 public class TextureGL extends ObjectGL {
     private int texture;
@@ -22,9 +27,23 @@ public class TextureGL extends ObjectGL {
         }
     }
 
-    // TODO
     public static TextureGL newTexture(String texture) {
-        InputStream is = App.getInputStream("textures/" + texture);
-        PNGDecoder decoder = new PNGDecoder(is);
+        try (ByteArrayOutputStream bao = new ByteArrayOutputStream();
+             InputStream is = App.getInputStream("textures/" + texture)) {
+
+            int read = is.read();
+            while (read != -1) {
+                bao.write(read);
+                read = is.read();
+            }
+
+            byte[] image = bao.toByteArray();
+            bao.close();
+            BufferedImage bufferedImage = ImageIO.read(new ByteArrayInputStream(image));
+
+            int[] pixels
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
